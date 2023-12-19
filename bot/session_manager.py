76b9c0c23016c464a95455f5f82ab7b273025c05
kbjcx <>
2023-12-19 17:59:13 +0800

    a

--- bot/session_manager.py
+++ bot/session_manager.py
@@ -4,4 +4,33 @@ from config import conf
 
 
 class Session(object):
-    pass
+    def __init__(self, session_id, system_prompt=None):
+        self.session_id = session_id
+        self.messages = []
+        if system_prompt is None:
+            self.system_prompt = conf().get("character_desc", "")
+        else:
+            self.system_prompt = system_prompt
+        
+
+    def reset(self):
+        """
+        reset the session
+        """
+        system_item = {"role": "system", "content": self.system_prompt}
+        self.messages = [system_item]
+
+    def set_system_prompt(self, system_prompt):
+        self.system_prompt = system_prompt
+        self.reset()
+
+    def add_query(self, query):
+        user_item = {"role": "user", "content": query}
+        self.messages.append(user_item)
+
+    def add_reply(self, reply):
+        assistant_item = {"role": "assistant", "content": reply}
+        self.messages.append(assistant_item)
+
+    # TODO
+        
