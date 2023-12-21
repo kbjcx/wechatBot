6ac4edd23bad929a5137cceb08278f55d8f77ac0
kbjcx <lu.kbjcx@gmail.com>
2023-12-21 22:19:20 +0800

    1221

---
+++ common/const.py
@@ -0,0 +1,19 @@
+# bot type
+OPEN_AI = "openAI"
+CHATGPT = "chatGPT"
+BAIDU = "baidu"
+GEMINI = "gemini"
+
+# model
+GPT35 = "gpt-3.5-turbo"
+GPT4 = "gpt-4"
+GPT4_TURBO_PREVIEW = "gpt-4-1106-preview"
+GPT4_VISION_PREVIEW = "gpt-4-vision-preview"
+WHISPER_1 = "whisper-1"
+TTS_1 = "tts-1"
+TTS_1_HD = "tts-1-hd"
+
+MODEL_LIST = [GPT35, "gpt-3.5-turbo-16k", GPT4, "wenxin", "wenxin-4", "gpt-4-turbo", GPT4_TURBO_PREVIEW, GEMINI]
+
+# channel
+FEISHU = "feishu"
