7e61a79836dba4ea9c5dec1b95e22804807515c1
kbjcx <lu.kbjcx@gmail.com>
2023-12-21 14:39:54 +0800

    session

--- bot/session_manager.py
+++ bot/session_manager.py
@@ -11,7 +11,6 @@ class Session(object):
             self.system_prompt = conf().get("character_desc", "")
         else:
             self.system_prompt = system_prompt
-        
 
     def reset(self):
         """
@@ -32,5 +31,64 @@ class Session(object):
         assistant_item = {"role": "assistant", "content": reply}
         self.messages.append(assistant_item)
 
-    # TODO
-        
+    def discard_exceeding(self, max_tokens=None, cur_tokens=None):
+        raise NotImplementedError
+
+    def calc_tokens(self):
+        raise NotImplementedError
+
+
+class SessionManager(object):
+    def __init__(self, sessioncls, **session_args):
+        if conf().get("expires_in_seconds"):
+            sessions = ExpiredDict(conf().get("expires_in_seconds"))
+        else:
+            sessions = dict()
+
+        self.sessions = sessions
+        self.session_args = session_args
+        self.sessioncls = sessioncls
+
+    def build_session(self, session_id, system_prompt=None):
+        """
+        如果session_id不在sessions中，创建一个新的session并添加到sessions中
+        如果system_prompt不为空，会更新session的system_prompt并重置session
+        """
+        if session_id is None:
+            return self.sessioncls(session_id, system_prompt, **self.session_args)
+        elif session_id not in self.sessions:
+            self.sessions[session_id] = self.sessioncls(session_id, system_prompt, **self.session_args)
+
+        session = self.sessions[session_id]
+        return session
+
+    def session_query(self, query, session_id):
+        session = self.build_session(session_id)
+        session.add_query(query)
+
+        try:
+            max_tokens = conf().get("conversation_max_tokens", 1000)
+            total_tokens = session.discard_exceeding(max_tokens, None)
+            logger.debug("prompt tokens used: {}".format(total_tokens))
+        except Exception as e:
+            logger.warning("Exception when counting tokens precisely for prompt: {}".format(str(e)))
+        return session
+
+    def session_reply(self, reply, session_id, total_tokens=None):
+        session = self.build_session(session_id)
+        session.add_reply(reply)
+
+        try:
+            max_tokens = conf().get("conversation_max_tokens", 1000)
+            tokens_cnt = session.discard_exceeding(max_tokens, total_tokens)
+            logger.debug("raw total tokens={}, save session tokens={}".format(total_tokens, tokens_cnt))
+        except Exception as e:
+            logger.warning("Exception when counting tokens precisely for session: {}".format(str(e)))
+        return session
+
+    def clear_session(self, session_id):
+        if session_id in self.sessions:
+            del self.sessions[session_id]
+
+    def clear_all_session(self):
+        self.sessions.clear()
